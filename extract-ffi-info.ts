341c9a44f5760df8aac6ac1d4218f24ed90efe29
glebbash <glebbash@gmail.com>
2022-08-02 21:03:47 +0000

    refactor: split up into modules

---
+++ extract-ffi-info.ts
@@ -0,0 +1,282 @@
+import { CEnum, CFunction, CSymbol, CType, CTypeDef } from "./types.ts";
+
+export type LibInfo = {
+  name: string;
+  prefix: string;
+  headersPath: string;
+  typeDefs: Map<string, TypeDef>;
+};
+
+export type TypeDef = {
+  originalName: string;
+  location: string;
+  type: TypeInfo;
+};
+
+export type EnumDef = TypeDef & {
+  values: { name: string; value: number }[];
+};
+
+export type TypeInfo = { tsType: string; nativeType: string };
+
+export type FunctionDef = {
+  fullName: string;
+  location: string;
+  parameters: { name: string; type: TypeInfo }[];
+  result: TypeInfo;
+};
+
+export function extractEnums(
+  lib: LibInfo,
+  symbols: CSymbol[],
+): Map<string, EnumDef> {
+  const enums = symbols.filter((s): s is CEnum => s.tag === "enum");
+  console.log("Total enums:", enums.length);
+
+  return new Map(
+    enums.map((e) => {
+      const mappedName = mapName(lib, e.name);
+      return [mappedName, {
+        originalName: e.name,
+        location: linkLocationToSource(e.location, lib.headersPath),
+        type: { tsType: `${lib.name}.${mappedName}`, nativeType: "i32" },
+        values: e.fields.map((v) => ({ name: v.name, value: v.value })),
+      }];
+    }),
+  );
+}
+
+export function extractTypeDefs(
+  lib: LibInfo,
+  symbols: CSymbol[],
+): Map<string, TypeDef> {
+  lib = { ...lib, typeDefs: new Map() };
+
+  const typeDefs = symbols.filter((s): s is CTypeDef => s.tag === "typedef");
+  console.log("Total types:", typeDefs.length);
+
+  for (const t of typeDefs) {
+    const mappedName = mapName(lib, t.name);
+    lib.typeDefs.set(mappedName, {
+      originalName: t.name,
+      location: linkLocationToSource(t.location, lib.headersPath),
+      type: getTypeInfo(t.type, mappedName, lib),
+    });
+  }
+
+  return lib.typeDefs;
+}
+
+export function extractFunctions(
+  lib: LibInfo,
+  symbols: CSymbol[],
+  exposedFunctions: string[],
+): Map<string, FunctionDef> {
+  const allFunctions = symbols.filter((s): s is CFunction =>
+    s.tag === "function"
+  );
+  const uniqueFunctions = uniqueByKey(allFunctions, "name");
+  const nonInlinedFunctions = uniqueFunctions.filter((f) => !f.inline);
+  const functions = nonInlinedFunctions.filter((f) =>
+    exposedFunctions.includes(f.name as never)
+  );
+  console.log("Total functions:", functions.length);
+
+  return new Map(functions.map((f) => [
+    mapName(lib, f.name),
+    {
+      fullName: f.name,
+      location: linkLocationToSource(f.location, lib.headersPath),
+      parameters: f.parameters.map((p, index) => ({
+        name: p.name || "_" + index,
+        type: getTypeInfo(p.type, null, lib),
+      })),
+      result: getTypeInfo(f["return-type"], null, lib),
+    },
+  ]));
+}
+
+function uniqueByKey<T>(values: T[], key: keyof T): T[] {
+  const seen = new Set<T[keyof T]>();
+  const result: T[] = [];
+
+  for (const value of values) {
+    const keyValue = value[key];
+    if (!seen.has(keyValue)) {
+      seen.add(keyValue);
+      result.push(value);
+    }
+  }
+
+  return result;
+}
+
+function linkLocationToSource(
+  location: string,
+  baseSourcePath: string,
+): string {
+  location = location.split(" <Spelling=")[0];
+  location = fixLine(location);
+
+  if (location.startsWith("/usr/include/")) {
+    return baseSourcePath + location.slice("/usr/include/".length);
+  }
+
+  const LOCAL_FILE_SEP = "/./";
+  const sepIndex = location.indexOf(LOCAL_FILE_SEP);
+  if (sepIndex !== -1) {
+    return baseSourcePath + location.slice(sepIndex + LOCAL_FILE_SEP.length);
+  }
+
+  return location;
+}
+
+function fixLine(str: string): string {
+  const [path, line] = str.split(":");
+  return path + "#L" + line;
+}
+
+function getTypeInfo(
+  type: CType,
+  name: string | null,
+  lib: LibInfo,
+): TypeInfo {
+  if (type.tag === ":enum") {
+    return {
+      tsType: `${lib.prefix}.${mapName(lib, type.name)}`,
+      nativeType: "i32",
+    };
+  }
+
+  if (type.tag === ":pointer") {
+    if (name === null) {
+      const rec = getTypeInfo(type.type, null, lib);
+
+      return { tsType: `Pointer<${rec.tsType}>`, nativeType: "pointer" };
+    }
+
+    return { tsType: `Pointer<"${name}">`, nativeType: "pointer" };
+  }
+
+  if (type.tag === ":function-pointer") {
+    return {
+      tsType: name !== null ? `FnPointer<"${name}">` : "FnPointer",
+      nativeType: "function",
+    };
+  }
+
+  if (type.tag === ":struct") {
+    if (name !== null && name !== type.name) {
+      throw new Error(
+        "Struct has multiple names: " + JSON.stringify({ type, name }),
+      );
+    }
+
+    return { tsType: `StructPointer<"${type.name}">`, nativeType: "pointer" };
+  }
+
+  if (type.tag === "struct") {
+    if (name === null) {
+      throw new Error("Struct does not have a name: " + JSON.stringify(type));
+    }
+
+    return { tsType: `StructPointer<"${name}">`, nativeType: "pointer" };
+  }
+
+  if (type.tag === ":void") {
+    return { tsType: "void", nativeType: "void" };
+  }
+
+  if (type.tag === "size_t") {
+    return { tsType: `number`, nativeType: "usize" };
+  }
+
+  if (
+    (type.tag === ":char" && type["bit-size"] === 8)
+  ) {
+    return { tsType: `number`, nativeType: "i8" };
+  }
+
+  if (
+    type.tag === "uint8_t" ||
+    (type.tag === ":unsigned-char" && type["bit-size"] === 8)
+  ) {
+    return { tsType: `number`, nativeType: "u8" };
+  }
+
+  if (type.tag === ":int" && type["bit-size"] === 32) {
+    return { tsType: `number`, nativeType: "i32" };
+  }
+
+  if (
+    type.tag === "uint32_t" ||
+    (type.tag === ":unsigned-int" && type["bit-size"] === 32)
+  ) {
+    return { tsType: `number`, nativeType: "u32" };
+  }
+
+  if (
+    (type.tag === "int64_t") ||
+    (type.tag === ":long-long" && type["bit-size"] === 64)
+  ) {
+    return { tsType: `bigint`, nativeType: "i64" };
+  }
+
+  if (type.tag === ":double" && type["bit-size"] === 64) {
+    return { tsType: `bigint`, nativeType: "f64" };
+  }
+
+  if (
+    type.tag === "uint64_t" ||
+    (type.tag === ":unsigned-long-long" && type["bit-size"] === 64)
+  ) {
+    return { tsType: `bigint`, nativeType: "u64" };
+  }
+
+  // TODO: check if this works
+  if (type.tag === "__builtin_va_list") {
+    return {
+      tsType: "bigint",
+      nativeType: "pointer",
+    };
+  }
+
+  const typeName = mapName(lib, type.tag);
+
+  const typeDef = lib.typeDefs.get(typeName);
+
+  if (typeDef === undefined) {
+    throw new Error(`Unknown type ${JSON.stringify({ type, name })}`);
+  }
+
+  return {
+    tsType: `${lib.name}.${typeName}`,
+    nativeType: typeDef.type.nativeType,
+  };
+
+  // if (type.tag.startsWith(lib.prefix)) {
+  //   const typeName = type.tag.substring(lib.prefix.length);
+
+  //   if (lib.typeDefs.has(typeName)) {
+  //     return {
+  //       tsType: `${lib.name}.${typeName}`,
+  //       nativeType: lib.typeDefs.get(typeName)!.type.nativeType,
+  //     };
+  //   }
+
+  //   if (lib.enums.has(typeName)) {
+  //     return {
+  //       tsType: `${lib.name}.${typeName}`,
+  //       nativeType: "i32",
+  //     };
+  //   }
+
+  //   throw new Error(
+  //     "Unexpected typedef: " + JSON.stringify({ type, name }),
+  //   );
+  // }
+}
+
+function mapName(lib: LibInfo, name: string): string {
+  return name.slice(lib.prefix.length);
+}
---
+++ generate-sources.ts
@@ -0,0 +1,169 @@
+import { m } from "https://raw.githubusercontent.com/glebbash/multiline-str/master/src/multiline-str.ts";
+import { EnumDef, FunctionDef, LibInfo, TypeDef } from "./extract-ffi-info.ts";
+
+export type LibSources = {
+  mod: string;
+  types: string;
+  symbols: string;
+  safeFFI: string;
+};
+
+export function generateSources(
+  lib: LibInfo,
+  typeDefs: Map<string, TypeDef>,
+  enums: Map<string, EnumDef>,
+  functions: Map<string, FunctionDef>,
+): LibSources {
+  const modSource = buildMod(lib);
+  const typesSource = buildTypes(lib, typeDefs, enums, functions);
+  const symbolsSource = buildSymbols(lib, functions);
+  const safeFFISource = buildSafeFFI();
+
+  return {
+    mod: modSource,
+    types: typesSource,
+    symbols: symbolsSource,
+    safeFFI: safeFFISource,
+  };
+}
+
+function buildSymbols(
+  ctx: LibInfo,
+  functions: Map<string, FunctionDef>,
+): string {
+  const symbolsGen = [...functions.entries()].map(([name, f]) => {
+    const parameters = f.parameters
+      .map((p) => `"${p.type.nativeType}"`)
+      .join(", ");
+
+    return m`
+        ${name}: {
+          name: "${f.fullName}",
+          parameters: [${parameters}],
+          result: "${f.result.nativeType}"
+        }
+      `;
+  }).join(",\n");
+
+  return m`
+    export const ${ctx.name}_SYMBOLS = {
+    ${symbolsGen}
+    } as const;
+
+    `;
+}
+
+function buildTypes(
+  ctx: LibInfo,
+  typeDefs: Map<string, TypeDef>,
+  enums: Map<string, EnumDef>,
+  functions: Map<string, FunctionDef>,
+): string {
+  const typeDefsSource = buildTypeDefs(typeDefs);
+  const enumsSource = buildEnums(enums);
+  const functionsSource = buildFunctions(functions);
+
+  const namespaceSource = m`
+    // deno-lint-ignore-file
+    import { Pointer, FnPointer, StructPointer } from "./safe-ffi.ts";
+
+    export namespace ${ctx.name} {
+    ${typeDefsSource}
+
+    ${enumsSource}
+
+    ${functionsSource}
+
+      export declare function close(): void;
+    }
+
+    `;
+
+  return namespaceSource;
+}
+
+function buildMod(ctx: LibInfo): string {
+  return m`
+    import { ${ctx.name} } from "./types.ts";
+    import { ${ctx.name}_SYMBOLS } from "./symbols.ts";
+
+    export * from "./safe-ffi.ts";
+    export type { ${ctx.name} };
+
+    export function load${ctx.name}(path: string): typeof ${ctx.name} {
+      const lib = Deno.dlopen(path, ${ctx.name}_SYMBOLS);
+
+      return { ...lib.symbols, close: () => lib.close() } as never;
+    }
+
+    `;
+}
+
+function buildSafeFFI() {
+  return m`
+    // deno-lint-ignore-file
+    export type Opaque<BaseType, BrandType = unknown> = BaseType & {
+      readonly [Symbols.base]: BaseType;
+      readonly [Symbols.brand]: BrandType;
+    };
+
+    export type Pointer<T = string> = Opaque<bigint, T>;
+    export type FnPointer<T = string> = Pointer<T>;
+    export type StructPointer<T = string> = Pointer<T>;
+
+    namespace Symbols {
+      export declare const base: unique symbol;
+      export declare const brand: unique symbol;
+    }
+
+    export function alloc<T>(): Pointer<T> {
+      return Deno.UnsafePointer.of(new BigUint64Array(1)) as Pointer<T>;
+    }
+    
+    export function cstr(str: string) {
+      return Deno.UnsafePointer.of(
+        new TextEncoder().encode(str + "\\0"),
+      ) as Pointer<number>;
+    }
+    
+    export function readCString(ptr: Pointer<number>): string {
+      return new Deno.UnsafePointerView(ptr).getCString();
+    }
+
+    `;
+}
+
+function buildTypeDefs(typeDefs: Map<string, TypeDef>): string {
+  const typeDefsSource = [...typeDefs.entries()].map(([name, t]) => {
+    return `  /** ${t.location} */\n` +
+      `  export type ${name} = ${t.type.tsType};`;
+  }).join("\n\n");
+
+  return typeDefsSource;
+}
+
+function buildEnums(enums: Map<string, EnumDef>): string {
+  const enumsSource = [...enums.entries()].map(([name, e]) => {
+    const fieldsGen = e.values
+      .map((f) => `    ${f.name} = ${f.value}`)
+      .join(",\n");
+
+    return `  /** ${e.location} */\n` +
+      `  export enum ${name} {\n` +
+      `${fieldsGen},\n` +
+      `  }`;
+  }).join("\n\n");
+
+  return enumsSource;
+}
+
+function buildFunctions(functions: Map<string, FunctionDef>): string {
+  const functionsSource = [...functions.entries()].map(([name, f]) => {
+    return `  /** ${f.location} */\n` +
+      `  ${`export declare function ${name}(${
+        f.parameters.map((p) => `${p.name}: ${p.type.tsType}`).join(", ")
+      }): ${f.result.tsType};`}`;
+  }).join("\n\n");
+
+  return functionsSource;
+}
